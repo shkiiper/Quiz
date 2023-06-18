32fdf5ad1365bc0f1758eac630b19360def5c02c
User <user@MacBook-Pro.local>
2023-06-18 12:50:38 +0600

    add commit

--- Quiz/models.py
+++ Quiz/models.py
@@ -67,35 +67,38 @@ class Question(models.Model):
         return self.question_text
 
     def save(self, *args, **kwargs):
-        if self.correct_answer == self.user_answer:
+        if self.correct_answer == self.user_answer: #Проверяется, совпадает ли значение correct_answer с user_answer объекта Question.
+            # Если да, то устанавливается значение is_correct в True, иначе остается False.
             self.is_correct = True
         super().save(*args, **kwargs)
 
-        testing = self.test
-        testing.total_question = Question.objects.filter(test=testing).count()
-        testing.total_correct_answer = Question.objects.filter(test=testing, is_correct=True).count()
+        testing = self.test #Получается связанный объект Testing через поле test.
+        testing.total_question = Question.objects.filter(test=testing).count() #Выполняются запросы к базе данных для подсчета общего количества вопросов
+        testing.total_correct_answer = Question.objects.filter(test=testing, is_correct=True).count() #и общего количества правильных ответов
         testing.save()
 
-        if testing.total_correct_answer >= 10:
+        if testing.total_correct_answer >= 10: #Если total_correct_answer в объекте Testing больше или равно 10, то устанавливается status в True для связанного объекта Section.
             section = testing.section
             section.status = True
             section.save()
 
-        current_user = User.objects.first()
+        current_user = User.objects.first() #получаем текущего пользователя
         if current_user:
-            section = testing.section
+            section = testing.section # получаем id текушего раздела
             result, created = Result.objects.get_or_create(
-                user=current_user,
-                section_name=section.name,
+                #Выполняется метод get_or_create модели Result для создания или получения объекта Result.
+                # Если объект был создан, то значение created будет True, иначе False.
+                user=current_user, # присваиваем значение current_user в поле user модели Result
+                section_name=section.name, # присваиваем значение name в поле section_name модели Result
                 defaults={
-                    'status': section.status,
-                    'total_question': testing.total_question,
-                    'total_correct_answer': testing.total_correct_answer,
-                    'section_id': section.id,
+                    'status': section.status,# присваиваем значение status в поле status модели Result
+                    'total_question': testing.total_question,## присваиваем значение total_question в поле total_question модели Result
+                    'total_correct_answer': testing.total_correct_answer,# присваиваем значение total_correct_answer в поле total_correct_answer модели Result
+                    'section_id': section.id,# присваиваем значение id в поле section_id модели Result
                 }
             )
 
-            if not created:
+            if not created:#Если объект Result не был создан (т.е., created равно False), значения обновляются и сохраняются в базе данных.
                 result.status = section.status
                 result.total_question = testing.total_question
                 result.total_correct_answer = testing.total_correct_answer
@@ -103,6 +106,10 @@ class Question(models.Model):
                 result.save()
 
 
+
+#Сигнал post_save является одним из сигналов в Django, который срабатывает после сохранения объекта модели.
+# Он позволяет выполнять определенные действия или обработку данных после того, как объект модели был сохранен.
+#это на всякий случай, если в методе save не найдется текущий пользоватекль
 @receiver(post_save, sender=Question, dispatch_uid="update_section_status")
 def update_section_status(sender, instance, created, **kwargs):
     if created:
